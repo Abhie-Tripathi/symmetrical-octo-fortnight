15f1dc78a0e79c6e44ccd982d8b0e3f81f573811
Abhinav Tripathi <abhiiii.tripathi@gmail.com>
2025-01-22 21:46:03 +0530

    Build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ReactQueryProvider from "@/provider/reactqueryprovider";
+import ReactQueryProvider from "@/provider/reactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
